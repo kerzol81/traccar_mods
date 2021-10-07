e3273e2caadaa997aadb9630164d700609ee7857
Z <kerenyi.nagy.zoltan@gmail.com>
2021-10-07 20:05:30 +0200

    Update dbmanager.py

--- dbmanager.py
+++ dbmanager.py
@@ -5,7 +5,7 @@ print ('[+] Connecting to local H2 database...')
 
 conn = jaydebeapi.connect('org.h2.Driver', 'jdbc:h2:/home/user/Documents/traccar/target/database', ["sa", ""], '/opt/traccar/lib/h2-1.4.200.jar')
 curs = conn.cursor()
-curs.execute('DELETE FROM tc_events WHERE eventTime < DATE(DATE_ADD(NOW(), INTERVAL -3 DAY)) LIMIT 10000')
+curs.execute('DELETE FROM traccar.tc_positions WHERE servertime < DATE(DATE_ADD(NOW(), INTERVAL -30 DAY)) LIMIT 10000')
 
 print ('[+] Database Cleared')
 
